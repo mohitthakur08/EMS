72cd4b4d9e7238fc4b7df4362f0427b1ffde5b1c
Mohit Thakur <officialmohit08@gmail.comgit config --global user.name Mohit>
2022-06-28 17:33:34 +0530

    login_signup_page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def destroy 
+    User.find(params[:id]).destroy
+    flash[success] = "User deleted"
+    redirect_to root_url
+  end
+end
