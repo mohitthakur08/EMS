61d25b7ab2a3045e527fa144f5329f81fb4a0bd8
mohit <mthakur@bestpeers.com>
2022-07-12 16:38:37 +0530

    modify

--- db/schema.rb
+++ db/schema.rb
@@ -15,19 +15,6 @@ ActiveRecord::Schema.define(version: 2022_07_11_064944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "daily_statuses", force: :cascade do |t|
-    t.string "p_status"
-    t.text "working_hours"
-    t.string "project_name"
-    t.text "task"
-    t.bigint "user_id", null: false
-    t.bigint "manager_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["manager_id"], name: "index_daily_statuses_on_manager_id"
-    t.index ["user_id"], name: "index_daily_statuses_on_user_id"
-  end
-
   create_table "leaves", force: :cascade do |t|
     t.date "leave_from"
     t.date "leave_to"
@@ -72,24 +59,20 @@ ActiveRecord::Schema.define(version: 2022_07_11_064944) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "first_name"
     t.string "last_name"
     t.string "password"
     t.integer "age"
     t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "unconfirmed_email"
     t.boolean "admin"
     t.integer "manager_id"
-    t.string "activation_digest"
-    t.boolean "activated", default: false
-    t.datetime "activated_at"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
@@ -99,8 +82,6 @@ ActiveRecord::Schema.define(version: 2022_07_11_064944) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "daily_statuses", "managers"
-  add_foreign_key "daily_statuses", "users"
   add_foreign_key "leaves", "users"
   add_foreign_key "managers", "users"
   add_foreign_key "projects", "users"
