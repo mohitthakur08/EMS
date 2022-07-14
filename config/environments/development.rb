7c98c351d00f74d34707d890f44bb4bda1b7138e
mohit <mthakur@bestpeers.com>
2022-07-14 19:29:35 +0530

    production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,11 +71,25 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  # config.action_mailer.raise_delivery_errors = false
+
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    user_name: 'postnow56@gmail.com',
+    password: 'enxtgdsaslkxlffs',
+    domain: "smtp.gmail.com"
+  }
  
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
